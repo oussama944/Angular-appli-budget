a345a760c37e78b640876638e239aed5104d019a
GSIB Oussama <Zyed.gsib@gmail.com>
2021-05-13 01:46:12 +0200

    Merge branch 'developpeur' of https://github.com/oussama944/C-Users-Oussama-Desktop-Angular-appli-budget into developpeur

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-=======
 import { BudgetItem } from 'src/shared/modules/budget-item.model';
 
->>>>>>> b73136b ("maj3")
 
 @Component({
   selector: 'app-main-page',
@@ -12,18 +9,12 @@ import { BudgetItem } from 'src/shared/modules/budget-item.model';
 })
 export class MainPageComponent implements OnInit {
 
-<<<<<<< HEAD
-=======
   budgetItems:BudgetItem[]=new Array<BudgetItem>();
 
->>>>>>> b73136b ("maj3")
   constructor() { }
 
   ngOnInit(): void {
   }
-<<<<<<< HEAD
-
-=======
   addItem(newItem:BudgetItem){
     this.budgetItems.push(newItem);
   }
@@ -31,5 +22,4 @@ export class MainPageComponent implements OnInit {
     let index=this.budgetItems.indexOf(item);
     this.budgetItems.splice(index,1);
   }
->>>>>>> b73136b ("maj3")
 }
